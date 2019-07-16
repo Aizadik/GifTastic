147e71e9825c238ca35d58042a4895fcc2eaf53d
Aizada Boposhova <aiszaman87@gmail.com>
2019-07-16 18:58:03 -0500

    addded all animes

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    //initial array of horror movies
+    var movies = ["Naruto", "Pokemon ", "Doraemon", "Tokyo Ghoul", "Parasite ", "Raki", "My Hero Acidamia", "Fairy Tail", "Sword Art Online"]
+    GIFArea = " "
+
+});
