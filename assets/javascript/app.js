bfc73ed000072ca00b108b5de9d9e78b02b2a2e8
Aizada Boposhova <aiszaman87@gmail.com>
2019-07-20 10:26:00 -0500

    get API response and dispay it after each btn is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -51,6 +51,52 @@ $(document).ready(function() {
 
     });
 
+    //==============Dislpay result what we get from API==============================
+    $(document).on('click', 'button', function() {
+        // Deleting the animes prior to adding new animes in order to avoid repetetive buttons
+        $('#GIFArea').empty();
+        // 'this' refers to the button that was clicked
+        var b = $(this).attr('data-name');
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + b + "&api_key=Cm28gxtrNhDU8x05Sa0h1kHt8UXNPYf0&limit=12";
+        console.log(queryURL);
+
+        //ajax call to get request
+        $.ajax({
+                url: queryURL,
+                method: 'GET'
+            })
+            //after the data comes back from the API
+            .done(function(response) {
+                console.log(response);
+                //Storing an array of results in the results variable
+                var results = response.data;
+                //Looping over every result item
+                for (var i = 0; i < results.length; i++) {
+                    //creating a div with the class item
+                    var gifDiv = $('<div class="item">');
+                    //storing the result items rating        
+                    var rating = results[i].rating;
+                    //creating an element to have the rating displayed
+                    var r = $('<p>').text("Rating: " + rating);
+                    //creating a image tag
+                    var gifImage = $('<img>');
+                    //giving the image tag an src attribute of a property pulled off the result item
+                    gifImage.attr('src', results[i].images.fixed_height_still.url)
+                        .attr('data-still', results[i].images.fixed_height_still.url)
+                        .attr('data-animate', results[i].images.fixed_height.url)
+                        .attr('data-state', "still")
+                        .addClass("showImage");
+                    //displaying the rating & image
+                    gifDiv.append(r)
+                        .append(gifImage);
+
+                    //adding the data                
+                    $('#GIFArea').prepend(gifDiv);
+                }
+
+            });
+    });
+
 
 
 
