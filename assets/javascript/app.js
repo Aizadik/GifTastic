f16907ed5495124faf7cb97ed00ce323bf4701c9
Aizada Boposhova <aiszaman87@gmail.com>
2019-07-20 10:11:37 -0500

    created list of animes, display them in the gui

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,34 @@
 $(document).ready(function() {
 
-    //initial array of horror movies
-    var animes = ["Naruto", "Pokemon ", "Doraemon", "Tokyo Ghoul", "Parasite ", "Raki", "My Hero Acidamia", "Fairy Tail", "Sword Art Online"]
-    GIFArea = " "
+    //list of animes
+    var animes = ["Naruto", "Pokemon ", "Doraemon", "Tokyo Ghoul", "Dragon Ball", "Raki", "My Hero Acidamia", "Fairy Tail", "Sword Art Online", "Charlotte", "Clannad", "Angel Beats!", "Mirai Nikki", "Toradora", "Kimi no Na wa", "Bleach", "Another", "K-On!"]
+    GIFArea = "";
+
+    //function for displaying anime data
+    function renderButtons() {
+        ""
+        //deleate the button before adding new button
+        $("#animes-view").empty();
+
+        //looping through the array of animes
+        for (var i = 0; i < animes.length; i++) {
+
+            //dynamically generate buttons for each movie in the array.
+            //this code $("<button>") is all jquery needs to create the start and end tag. (<button></button>)
+            var a = $('<button>');
+            //Adding a class
+            a.addClass('anime');
+
+            //adding a data-attribute with a value of the television at index i
+            a.attr('data-name', animes[i]);
+            //providing the button's text with a value of the anime at index i
+            a.text(animes[i]);
+            //adding the button the html
+            $("#animes-view").append(a);
+        }
+
+        $("#anime-input").focus();
+
+    }
 
 });
